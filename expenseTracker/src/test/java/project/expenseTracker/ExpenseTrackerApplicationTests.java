5be3ad32b8a858e810f9d3b765aa638464931b4e
brkybzkrt <b9bozkurt959@gmail.com>
2021-08-15 21:02:15 +0300

    first commit

---
+++ expenseTracker/src/test/java/project/expenseTracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.expenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
