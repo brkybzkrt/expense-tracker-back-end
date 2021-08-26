3884ed79bec1aa404f95b9d003c00ce933490a55
brkybzkrt <b9bozkurt959@gmail.com>
2021-08-26 17:09:40 +0300

    Dtos of Category added

---
+++ expenseTracker/src/main/java/project/expenseTracker/entities/dtos/categoryDtos/AddCategoryDto.java
@@ -0,0 +1,11 @@
+package project.expenseTracker.entities.dtos.categoryDtos;
+
+import lombok.Data;
+
+@Data
+public class AddCategoryDto {
+
+	
+	private int id;
+	private String name;
+}
---
+++ expenseTracker/src/main/java/project/expenseTracker/entities/dtos/categoryDtos/UpdateCategoryDto.java
@@ -0,0 +1,10 @@
+package project.expenseTracker.entities.dtos.categoryDtos;
+
+import lombok.Data;
+
+@Data
+public class UpdateCategoryDto {
+
+	private int id;
+	private String name;
+}
